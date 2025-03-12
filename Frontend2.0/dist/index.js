460b27a95f0bd89db4245badd6396f30a940c8cc
lulocas <andradeps434@gmail.com>
2025-03-12 11:01:58 -0300

    adicionado tudo em um arquivo

--- Frontend2.0/dist/index.js
+++ Frontend2.0/dist/index.js
@@ -1,9 +1,244 @@
-import { cadastrar, botao } from "./cadastro.js";
-import { inputhabilidades, escreverHab, mudarNome, botaoSalvar, salvar } from "./editarC.js";
-import { mudarDados } from "./perfilC.js";
+"use strict";
+// Classe candidato
+class CandidatoIndex {
+    constructor(nome, email, senha, cpf, idade, estado, cep, descricao, formacao, experiencia, habilidades) {
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.estado = estado;
+        this.cep = cep;
+        this.descricao = descricao;
+        this.formacao = formacao;
+        this.experiencia = experiencia;
+        this.habilidades = habilidades;
+    }
+    getNome() {
+        return this.nome;
+    }
+    getEmail() {
+        return this.email;
+    }
+    getSenha() {
+        return this.senha;
+    }
+    getCpf() {
+        return this.cpf;
+    }
+    getIdade() {
+        return this.idade;
+    }
+    getEstado() {
+        return this.estado;
+    }
+    getCep() {
+        return this.cep;
+    }
+    getDescricao() {
+        return this.descricao;
+    }
+    getFormacao() {
+        return this.formacao;
+    }
+    getExperiencia() {
+        return this.experiencia;
+    }
+    getHabilidades() {
+        return this.habilidades;
+    }
+    setNome(nome) {
+        this.nome = nome;
+    }
+    setEmail(email) {
+        this.email = email;
+    }
+    setSenha(senha) {
+        this.senha = senha;
+    }
+    setCpf(cpf) {
+        this.cpf = cpf;
+    }
+    setIdade(idade) {
+        this.idade = idade;
+    }
+    setEstado(estado) {
+        this.estado = estado;
+    }
+    setCep(cep) {
+        this.cep = cep;
+    }
+    setDescricao(descricao) {
+        this.descricao = descricao;
+    }
+    setFormacao(formacao) {
+        this.formacao = formacao;
+    }
+    setExperiencia(experiencia) {
+        this.experiencia = experiencia;
+    }
+    setHabilidades(habilidades) {
+        this.habilidades = habilidades;
+    }
+}
+// Cadastro
+const inputNomeIndex = document.getElementById('nome');
+const inputEmailIndex = document.getElementById('email');
+const inputSenhaIndex = document.getElementById('senha');
+const inputConfirmaIndex = document.getElementById('confirmarSenha');
+const candidatoInputIndex = document.getElementById('candidatoBotao');
+const empresaInputIndex = document.getElementById('empresaBotao');
+const botaoIndex = document.getElementById('botaoCadastrar');
+const seuNomeElementIndex = document.getElementById('seuNome');
+const seuNomeElement = document.getElementById('seuNome');
+let usuarioIndex;
+let nomeIndex;
+let emailIndex;
+let senhaIndex;
+function validar() {
+    if (inputNomeIndex.value === '') {
+        alert('Nome é obrigatório');
+        return false;
+    }
+    if (inputEmailIndex.value === '') {
+        alert('Email é obrigatório');
+        return false;
+    }
+    if (inputSenhaIndex.value === '') {
+        alert('Senha é obrigatório');
+        return false;
+    }
+    if (inputConfirmaIndex.value === '') {
+        alert('Confirmação de senha é obrigatória');
+        return false;
+    }
+    if (inputSenhaIndex.value !== inputConfirmaIndex.value) {
+        alert('Senhas não conferem');
+        return false;
+    }
+    if (!candidatoInputIndex.checked && !empresaInputIndex.checked) {
+        alert('Selecione um tipo de cadastro');
+        return false;
+    }
+    return true;
+}
+function cadastrar() {
+    if (validar()) {
+        alert('Cadastro realizado com sucesso!');
+        if (candidatoInputIndex.checked) {
+            usuarioIndex = 'candidato';
+            alert(`Usuário ${usuarioIndex} cadastrado com sucesso!`);
+            window.location.href = 'edicaoC.html';
+        }
+        else if (empresaInputIndex.checked) {
+            usuarioIndex = 'empresa';
+            alert(`Usuário ${usuarioIndex} cadastrado com sucesso!`);
+            window.location.href = 'edicaoE.html';
+        }
+        nomeIndex = inputNomeIndex.value;
+        emailIndex = inputEmailIndex.value;
+        senhaIndex = inputSenhaIndex.value;
+        localStorage.setItem('nomeUsuario', nomeIndex);
+    }
+}
+// Edicao C
+const inputcpf = document.getElementById('cpf');
+const inputidade = document.getElementById('idade');
+const inputestado = document.getElementById('estado');
+const inputcep = document.getElementById('cep');
+const inputdescricao = document.getElementById('descricao');
+const inputformacao = document.getElementById('formacao');
+const inputexperiencia = document.getElementById('experiencia');
+const inputhabilidades = document.getElementById('habilidades');
+const botaoSalvar = document.getElementById('botaoSalvar');
+const divHabilidades = document.getElementById('divHab');
+let candidato;
+let listaHab = [];
+function mudarNome(nome) {
+    if (seuNomeElement) {
+        seuNomeElement.textContent = nome;
+    }
+}
+function validarEdicao() {
+    if (inputcpf.value === '') {
+        alert('CPF é obrigatório');
+        return false;
+    }
+    if (inputidade.value === '') {
+        alert('Idade é obrigatório');
+        return false;
+    }
+    if (inputestado.value === '') {
+        alert('Estado é obrigatório');
+        return false;
+    }
+    if (inputcep.value === '') {
+        alert('CEP é obrigatório');
+        return false;
+    }
+    if (inputdescricao.value === '') {
+        alert('Descrição é obrigatório');
+        return false;
+    }
+    if (inputformacao.value === '') {
+        alert('Formação é obrigatório');
+        return false;
+    }
+    if (listaHab.length < 3) {
+        alert('Adicione pelo menos 3 habilidades');
+        return false;
+    }
+    return true;
+}
+function escreverHab() {
+    if (inputhabilidades.value !== '') {
+        criarHabilidade();
+        listaHab.push(inputhabilidades.value);
+        inputhabilidades.value = '';
+        console.log(listaHab);
+    }
+}
+function criarHabilidade() {
+    var divHabili = document.createElement('div');
+    divHabili.classList.add("habil");
+    divHabilidades.appendChild(divHabili);
+    var habilidade = document.createElement('p');
+    habilidade.textContent = inputhabilidades.value;
+    habilidade.classList.add("pHab");
+    divHabili.appendChild(habilidade);
+}
+function salvar(nome, email, senha) {
+    if (validarEdicao()) {
+        alert('Salvo com sucesso');
+        candidato = new CandidatoIndex(nome, email, senha, inputcpf.value, parseInt(inputidade.value), inputestado.value, inputcep.value, inputdescricao.value, inputformacao.value, inputexperiencia.value, listaHab);
+        window.location.href = 'perfilC.html';
+    }
+}
+// Perfil c
+const h3Nome = document.getElementById("seuNome");
+const pCpf = document.getElementById("cpf");
+const pidade = document.getElementById("idade");
+const pestado = document.getElementById("estado");
+const pcep = document.getElementById("cep");
+const pdescricao = document.getElementById("descricao");
+const pexperiencia = document.getElementById("experiencia");
+const pformacao = document.getElementById("formacao");
+function mudarDados() {
+    if (candidato) {
+        h3Nome.textContent = candidato.getNome();
+        pCpf.textContent = candidato.getCpf();
+        pidade.textContent = candidato.getIdade().toString();
+        pestado.textContent = candidato.getEstado();
+        pcep.textContent = candidato.getCep();
+        pdescricao.textContent = candidato.getDescricao();
+        pexperiencia.textContent = candidato.getExperiencia();
+        pformacao.textContent = candidato.getFormacao();
+    }
+}
+// Index parte principal
 document.addEventListener('DOMContentLoaded', () => {
-    if (botao) {
-        botao.addEventListener('click', cadastrar);
+    if (botaoIndex) {
+        botaoIndex.addEventListener('click', cadastrar);
     }
     const nomeUsuario = localStorage.getItem('nomeUsuario');
     if (nomeUsuario) {
@@ -19,7 +254,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
         if (botaoSalvar) {
-            botaoSalvar.addEventListener('click', salvar);
+            botaoSalvar.addEventListener('click', () => salvar(nomeIndex, emailIndex, senhaIndex));
         }
         if (window.location.pathname.endsWith('/perfilC.html')) {
             mudarDados();
--- Frontend2.0/src/index.ts
+++ Frontend2.0/src/index.ts
@@ -1,10 +1,291 @@
-import { cadastrar, botao} from "./cadastro.js";
-import { inputhabilidades, escreverHab, mudarNome, botaoSalvar, salvar } from "./editarC.js";
-import { mudarDados } from "./perfilC.js";
+// Classe candidato
+class CandidatoIndex{
+    private nome: string;
+    private email: string;
+    private senha: string;
+    private cpf: string;
+    private idade: number;
+    private estado: string;
+    private cep: string;
+    private descricao: string;
+    private formacao: string;
+    private experiencia: string;
+    private habilidades: string[];
 
+    constructor(nome: string, email: string, senha: string, cpf: string, idade: number, estado: string, cep: string, descricao: string, formacao: string, experiencia: string, habilidades: string[]){
+        this.nome = nome;
+        this.email = email;
+        this.senha = senha;
+        this.cpf = cpf;
+        this.idade = idade;
+        this.estado = estado;
+        this.cep = cep;
+        this.descricao = descricao;
+        this.formacao = formacao;
+        this.experiencia = experiencia;
+        this.habilidades = habilidades;
+    }
+
+    getNome(): string{
+        return this.nome;
+    }
+
+    getEmail(): string{
+        return this.email;
+    }
+
+    getSenha(): string{
+        return this.senha;
+    }
+
+    getCpf(): string{
+        return this.cpf;
+    }
+
+    getIdade(): number{
+        return this.idade;
+    }
+
+    getEstado(): string{
+        return this.estado;
+    }
+
+    getCep(): string{
+        return this.cep;
+    }
+
+    getDescricao(): string{
+        return this.descricao;
+    }
+
+    getFormacao(): string{
+        return this.formacao;
+    }
+
+    getExperiencia(): string{
+        return this.experiencia;
+    }
+
+    getHabilidades(): string[]{
+        return this.habilidades;
+    }
+
+    setNome(nome: string): void{
+        this.nome = nome;
+    }
+
+    setEmail(email: string): void{
+        this.email = email;
+    }
+
+    setSenha(senha: string): void{
+        this.senha = senha;
+    }
+
+    setCpf(cpf: string): void{
+        this.cpf = cpf;
+    }
+
+    setIdade(idade: number): void{
+        this.idade = idade;
+    }
+
+    setEstado(estado: string): void{
+        this.estado = estado;
+    }
+    
+    setCep(cep: string): void{
+        this.cep = cep;
+    }
+
+    setDescricao(descricao: string): void{
+        this.descricao = descricao;
+    }
+
+    setFormacao(formacao: string): void{
+        this.formacao = formacao;
+    }
+
+    setExperiencia(experiencia: string): void{
+        this.experiencia = experiencia;
+    }
+
+    setHabilidades(habilidades: string[]): void{
+        this.habilidades = habilidades;
+    }
+}
+
+
+// Cadastro
+const inputNomeIndex = document.getElementById('nome') as HTMLInputElement;
+const inputEmailIndex = document.getElementById('email') as HTMLInputElement;
+const inputSenhaIndex = document.getElementById('senha') as HTMLInputElement;
+const inputConfirmaIndex = document.getElementById('confirmarSenha') as HTMLInputElement;
+const candidatoInputIndex = document.getElementById('candidatoBotao') as HTMLInputElement;
+const empresaInputIndex = document.getElementById('empresaBotao') as HTMLInputElement;
+const botaoIndex = document.getElementById('botaoCadastrar') as HTMLButtonElement;
+const seuNomeElementIndex = document.getElementById('seuNome');
+const seuNomeElement = document.getElementById('seuNome');
+let usuarioIndex: string;
+let nomeIndex: string;
+let emailIndex: string;
+let senhaIndex: string;
+
+function validar(): boolean {
+    if (inputNomeIndex.value === '') {
+        alert('Nome é obrigatório');
+        return false;
+    }
+    if (inputEmailIndex.value === '') {
+        alert('Email é obrigatório');
+        return false;
+    }
+    if (inputSenhaIndex.value === '') {
+        alert('Senha é obrigatório');
+        return false;
+    }
+    if (inputConfirmaIndex.value === '') {
+        alert('Confirmação de senha é obrigatória');
+        return false;
+    }
+    if (inputSenhaIndex.value !== inputConfirmaIndex.value) {
+        alert('Senhas não conferem');
+        return false;
+    }
+    if (!candidatoInputIndex.checked && !empresaInputIndex.checked) {
+        alert('Selecione um tipo de cadastro');
+        return false;
+    }
+    return true;
+}
+
+function cadastrar() {
+    if (validar()) {
+        alert('Cadastro realizado com sucesso!');
+        if (candidatoInputIndex.checked) {
+            usuarioIndex = 'candidato';
+            alert(`Usuário ${usuarioIndex} cadastrado com sucesso!`);
+            window.location.href = 'edicaoC.html';
+        } else if (empresaInputIndex.checked) {
+            usuarioIndex = 'empresa';
+            alert(`Usuário ${usuarioIndex} cadastrado com sucesso!`);
+            window.location.href = 'edicaoE.html';
+        }
+        nomeIndex = inputNomeIndex.value;
+        emailIndex = inputEmailIndex.value;
+        senhaIndex = inputSenhaIndex.value;
+
+        localStorage.setItem('nomeUsuario', nomeIndex);
+    }
+}
+
+// Edicao C
+const inputcpf = document.getElementById('cpf') as HTMLInputElement;
+const inputidade = document.getElementById('idade') as HTMLInputElement;
+const inputestado = document.getElementById('estado') as HTMLInputElement;
+const inputcep = document.getElementById('cep') as HTMLInputElement;
+const inputdescricao = document.getElementById('descricao') as HTMLInputElement;
+const inputformacao = document.getElementById('formacao') as HTMLInputElement;
+const inputexperiencia = document.getElementById('experiencia') as HTMLInputElement;
+const inputhabilidades = document.getElementById('habilidades') as HTMLInputElement;
+const botaoSalvar = document.getElementById('botaoSalvar') as HTMLButtonElement;
+const divHabilidades = document.getElementById('divHab') as HTMLDivElement;
+let candidato: CandidatoIndex | undefined;
+let listaHab: string[] = [];
+
+function mudarNome(nome: string) {
+    if (seuNomeElement) {
+        seuNomeElement.textContent = nome;
+    } 
+}
+
+function validarEdicao(): boolean {
+    if (inputcpf.value === '') {
+        alert('CPF é obrigatório');
+        return false;
+    }
+    if (inputidade.value === '') {
+        alert('Idade é obrigatório');
+        return false;
+    }
+    if (inputestado.value === '') {
+        alert('Estado é obrigatório');
+        return false;
+    }
+    if (inputcep.value === '') {
+        alert('CEP é obrigatório');
+        return false;
+    }
+    if (inputdescricao.value === '') {
+        alert('Descrição é obrigatório');
+        return false;
+    }
+    if (inputformacao.value === '') {
+        alert('Formação é obrigatório');
+        return false;
+    }
+    if(listaHab.length < 3) {
+        alert('Adicione pelo menos 3 habilidades');
+        return false;
+    }
+    return true;
+}
+
+function escreverHab(){
+    if(inputhabilidades.value !== '') {
+        criarHabilidade();
+        listaHab.push(inputhabilidades.value);
+        inputhabilidades.value = '';
+        console.log(listaHab);
+    }   
+}
+
+function criarHabilidade() {
+    var divHabili = document.createElement('div');
+    divHabili.classList.add("habil");
+    divHabilidades.appendChild(divHabili);
+    var habilidade = document.createElement('p');
+    habilidade.textContent = inputhabilidades.value;
+    habilidade.classList.add("pHab");
+    divHabili.appendChild(habilidade);
+}
+
+function salvar(nome : string, email: string, senha: string) {
+    if(validarEdicao()){
+        alert('Salvo com sucesso');
+        candidato = new CandidatoIndex(nome, email, senha, inputcpf.value, parseInt(inputidade.value), inputestado.value, inputcep.value, inputdescricao.value, inputformacao.value, inputexperiencia.value, listaHab);
+        window.location.href = 'perfilC.html';
+    }
+}
+
+// Perfil c
+const h3Nome = document.getElementById("seuNome") as HTMLElement;
+const pCpf = document.getElementById("cpf") as HTMLElement;
+const pidade = document.getElementById("idade") as HTMLElement;
+const pestado = document.getElementById("estado") as HTMLElement;
+const pcep = document.getElementById("cep") as HTMLElement;
+const pdescricao = document.getElementById("descricao") as HTMLElement;
+const pexperiencia = document.getElementById("experiencia") as HTMLElement;
+const pformacao = document.getElementById("formacao") as HTMLElement;
+
+function mudarDados(){
+    
+    if (candidato) {
+        h3Nome.textContent = candidato.getNome();
+        pCpf.textContent = candidato.getCpf();
+        pidade.textContent = candidato.getIdade().toString();
+        pestado.textContent = candidato.getEstado();
+        pcep.textContent = candidato.getCep();
+        pdescricao.textContent = candidato.getDescricao();
+        pexperiencia.textContent = candidato.getExperiencia();
+        pformacao.textContent = candidato.getFormacao();
+    }
+}
+
+// Index parte principal
 document.addEventListener('DOMContentLoaded', () => {
-    if (botao) {
-        botao.addEventListener('click', cadastrar);
+    if (botaoIndex) {
+        botaoIndex.addEventListener('click', cadastrar);
     }
     const nomeUsuario = localStorage.getItem('nomeUsuario');
     if (nomeUsuario) {
@@ -20,7 +301,7 @@ document.addEventListener('DOMContentLoaded', () => {
             });
         }
         if(botaoSalvar){
-            botaoSalvar.addEventListener('click', salvar);
+            botaoSalvar.addEventListener('click', () => salvar(nomeIndex, emailIndex, senhaIndex));
         }
         if(window.location.pathname.endsWith('/perfilC.html')){
             mudarDados();
